fb94d4e6fc1292c1f78975e1a727b5d6c0ccb60c
Brian <brionace@gmail.com>
2022-04-05 01:09:54 +0100

    added type to category2

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -27,7 +27,7 @@ export default function Categories({ categories, portfolio }: CategoriesType) {
         const portfolio = await getPortfolio()
 
         categories.data.forEach((category: unknown) => {
-            const catId = category.id
+            const catId: number = category.id
             portfolio.data.forEach(project => {
                 if (!ids.includes(catId)) {
                     if (project.attributes.categories.data.some((e: number) => e.id === catId)) {
